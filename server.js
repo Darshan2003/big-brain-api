5d0666dd3e4d4c47d5ebefbc7b427716452a77fb
Darshan <darshanjain727@gmail.com>
2021-06-02 22:02:57 +0530

    minor fix

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 app.post('/imageUrl', (req, res) => {image.handleApiCall(req, res)})
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT ||  3000, ()=> {
+  console.log(`app is running on port ${process.env.PORT}`);
 })
